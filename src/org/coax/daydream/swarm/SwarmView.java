3f786ed6bd735cc286086b42021297cba02e2e57
coax75ohm <coax75ohm@gmail.com>
2014-09-02 10:26:02 -0500

    cleanup and fix math

--- src/org/coax/daydream/swarm/SwarmView.java
+++ src/org/coax/daydream/swarm/SwarmView.java
@@ -11,17 +11,17 @@ public class SwarmView extends SurfaceView implements SurfaceHolder.Callback {
 
     private int width;
     private int height;
-    private SurfaceHolder holder;
     private Canvas canvas;
+    private SurfaceHolder holder;
     
-    private static final int BEES     = 20;	// number of bees
-    private static final int REPEAT   = 3;	// number of time positions recorded
-    private static final int BEE_ACC  = 3;	// acceleration of bees
-    private static final int WASP_ACC = 5;	// maximum acceleration of wasp
-    private static final int BEE_VEL  = 11;	// maximum bee velocity
-    private static final int WASP_VEL = 12;	// maximum wasp velocity
-    private static final int BORDER   = 50;	// wasp won't go closer than this to the edges
-    private static final int DELAY    = 40;	// millisecond delay between updates
+    private static final int BEES     = 20;  // number of bees
+    private static final int REPEAT   = 3;   // number of time positions recorded
+    private static final int BEE_ACC  = 3;   // acceleration of bees
+    private static final int WASP_ACC = 5;   // maximum acceleration of wasp
+    private static final int BEE_VEL  = 11;  // maximum bee velocity
+    private static final int WASP_VEL = 12;  // maximum wasp velocity
+    private static final int BORDER   = 50;  // wasp won't go closer than this to the edges
+    private static final int DELAY    = 40;  // millisecond delay between updates
     
     int[] wasp_x = new int[2];
     int[] wasp_y = new int[2];
@@ -47,15 +47,15 @@ public class SwarmView extends SurfaceView implements SurfaceHolder.Callback {
     @Override
     public void surfaceCreated(SurfaceHolder sh) {
 
-    	new Thread() {
+        new Thread() {
             @Override
             public void run() {
         
-            	canvas = null;
+                canvas = null;
 
                 // initialize wasp
-            	wasp_x[0] = (int)(Math.random() * (float)((width - 2 * BORDER) + BORDER));
-                wasp_y[0] = (int)(Math.random() * (float)((height - 2 * BORDER) + BORDER));
+                wasp_x[0] = (int)(Math.random() * (width - 2 * BORDER)) + BORDER;
+                wasp_y[0] = (int)(Math.random() * (height - 2 * BORDER)) + BORDER;
                 wasp_x[1] = wasp_x[0];
                 wasp_y[1] = wasp_y[0];
                 
@@ -68,13 +68,13 @@ public class SwarmView extends SurfaceView implements SurfaceHolder.Callback {
 
                 // initialize bees
                 for(int i = 0; i < BEES; i++) {
-                	bee_x[i][0] = (int)(Math.random() * (float)width);
-                	bee_y[i][0] = (int)(Math.random() * (float)height);
-                	bee_x[i][1] = bee_x[i][0];
-                	bee_y[i][1] = bee_y[i][0];
-                	
-                	bee_xv[i] = randPlusMinus(7);
-                	bee_yv[i] = randPlusMinus(7);
+                    bee_x[i][0] = (int)(Math.random() * width);
+                    bee_y[i][0] = (int)(Math.random() * height);
+                    bee_x[i][1] = bee_x[i][0];
+                    bee_y[i][1] = bee_y[i][0];
+                    
+                    bee_xv[i] = randPlusMinus(7);
+                    bee_yv[i] = randPlusMinus(7);
                 }
 
                 bee_p = new Paint();
@@ -84,101 +84,101 @@ public class SwarmView extends SurfaceView implements SurfaceHolder.Callback {
                 // animation loop
                 while (true) {
                     canvas = holder.lockCanvas();
-                	 
-                	if (canvas == null) {
+                     
+                    if (canvas == null) {
                         break;
                     }
-                	
-                	// <=- wasp -=>
-                	// age the arrays
-                	wasp_x[1] = wasp_x[0];
-                	wasp_y[1] = wasp_y[0];
-                	
-                	// accelerate
-                	wasp_xv += randPlusMinus(WASP_ACC);
-                	wasp_yv += randPlusMinus(WASP_ACC);
-                	
-                	// speed limit checks
-                	if (wasp_xv > WASP_VEL) {
-                		wasp_xv = WASP_VEL;
-                	}
-                	if (wasp_xv < -WASP_VEL) {
-                		wasp_xv = -WASP_VEL;
-                	}
-                	if (wasp_yv > WASP_VEL) {
-                		wasp_yv = WASP_VEL;
-                	}
-                	if (wasp_yv < -WASP_VEL) {
-                		wasp_yv = -WASP_VEL;
-                	}
+                    
+                    // <=- wasp -=>
+                    // age the arrays
+                    wasp_x[1] = wasp_x[0];
+                    wasp_y[1] = wasp_y[0];
+                    
+                    // accelerate
+                    wasp_xv += randPlusMinus(WASP_ACC);
+                    wasp_yv += randPlusMinus(WASP_ACC);
+                    
+                    // speed limit checks
+                    if (wasp_xv > WASP_VEL) {
+                        wasp_xv = WASP_VEL;
+                    }
+                    if (wasp_xv < -WASP_VEL) {
+                        wasp_xv = -WASP_VEL;
+                    }
+                    if (wasp_yv > WASP_VEL) {
+                        wasp_yv = WASP_VEL;
+                    }
+                    if (wasp_yv < -WASP_VEL) {
+                        wasp_yv = -WASP_VEL;
+                    }
 
-                	// move
-                	wasp_x[0] = wasp_x[1] + wasp_xv;
-                	wasp_y[0] = wasp_y[1] + wasp_yv;
+                    // move
+                    wasp_x[0] = wasp_x[1] + wasp_xv;
+                    wasp_y[0] = wasp_y[1] + wasp_yv;
 
-                	// bounce checks
-                	if((wasp_x[0] < BORDER) || (wasp_x[0] > (width - BORDER - 1))) {
-                		wasp_xv = -wasp_xv;
-                		wasp_x[0] += wasp_xv;
-                	}
-                	if((wasp_y[0] < BORDER) || (wasp_y[0] > (height - BORDER - 1))) {
-                		wasp_yv = -wasp_yv;
-                		wasp_y[0] += wasp_yv;
-                	}
-                	
-                	// don't let things settle down
-                	bee_xv[(int)(Math.random() * BEES)] += randPlusMinus(3);
-                	bee_yv[(int)(Math.random() * BEES)] += randPlusMinus(3);
-                	
-                	// <=- bees -=>
-                    for(int i = 0; i < BEES; i++) {
-                    	// age the arrays
-                    	bee_x[i][2] = bee_x[i][1];
-                    	bee_x[i][1] = bee_x[i][0];
-                    	bee_y[i][2] = bee_y[i][1];
-                    	bee_y[i][1] = bee_y[i][0];
-                    	
-                    	// accelerate
-                    	dx = wasp_x[1] - bee_x[i][1];
-                    	dy = wasp_y[1] - bee_y[i][1];
-                    	distance = Math.abs(dx) + Math.abs(dy);	// approximation
-                    	if(distance < 1) {
-                    		distance = 1;
-                    	}
-                    	bee_xv[i] += (dx * BEE_ACC) / distance;
-                		bee_yv[i] += (dy * BEE_ACC) / distance;
-                	
-                    	// speed limit checks
-                    	if (bee_xv[i] > BEE_VEL) {
-                    		bee_xv[i] = BEE_VEL;
-                    	}
-                    	if (bee_xv[i] < -BEE_VEL) {
-                    		bee_xv[i] = -BEE_VEL;
-                    	}
-                    	if (bee_yv[i] > BEE_VEL) {
-                    		bee_yv[i] = BEE_VEL;
-                    	}
-                    	if (bee_yv[i] < -BEE_VEL) {
-                    		bee_yv[i] = -BEE_VEL;
-                    	}
-                    	
-                    	// move
-                    	bee_x[i][0] = bee_x[i][1] + bee_xv[i];
-                    	bee_y[i][0] = bee_y[i][1] + bee_yv[i];
+                    // bounce checks
+                    if((wasp_x[0] < BORDER) || (wasp_x[0] > (width - BORDER - 1))) {
+                        wasp_xv = -wasp_xv;
+                        wasp_x[0] += wasp_xv;
+                    }
+                    if((wasp_y[0] < BORDER) || (wasp_y[0] > (height - BORDER - 1))) {
+                        wasp_yv = -wasp_yv;
+                        wasp_y[0] += wasp_yv;
                     }
                     
-                	// erase previous, draw current
-                	canvas.drawColor(Color.BLACK);
-                	                	
-                    // wasp
-                	canvas.drawLine(wasp_x[0], wasp_y[0], wasp_x[1], wasp_y[1], wasp_p);
-                	
-                	//bees
-                	for(int i = 0; i < BEES; i++) {
-                		canvas.drawLine(bee_x[i][1], bee_y[i][1], bee_x[i][2], bee_y[i][2], bee_p);
-                		canvas.drawLine(bee_x[i][0], bee_y[i][0], bee_x[i][1], bee_y[i][1], bee_p);
+                    // don't let things settle down
+                    bee_xv[(int)(Math.random() * BEES)] += randPlusMinus(3);
+                    bee_yv[(int)(Math.random() * BEES)] += randPlusMinus(3);
+                    
+                    // <=- bees -=>
+                    for(int i = 0; i < BEES; i++) {
+                        // age the arrays
+                        bee_x[i][2] = bee_x[i][1];
+                        bee_x[i][1] = bee_x[i][0];
+                        bee_y[i][2] = bee_y[i][1];
+                        bee_y[i][1] = bee_y[i][0];
+                        
+                        // accelerate
+                        dx = wasp_x[1] - bee_x[i][1];
+                        dy = wasp_y[1] - bee_y[i][1];
+                        distance = Math.abs(dx) + Math.abs(dy);  // approximation
+                        if(distance < 1) {
+                            distance = 1;
+                        }
+                        bee_xv[i] += (dx * BEE_ACC) / distance;
+                        bee_yv[i] += (dy * BEE_ACC) / distance;
+                    
+                        // speed limit checks
+                        if (bee_xv[i] > BEE_VEL) {
+                            bee_xv[i] = BEE_VEL;
+                        }
+                        if (bee_xv[i] < -BEE_VEL) {
+                            bee_xv[i] = -BEE_VEL;
+                        }
+                        if (bee_yv[i] > BEE_VEL) {
+                            bee_yv[i] = BEE_VEL;
+                        }
+                        if (bee_yv[i] < -BEE_VEL) {
+                            bee_yv[i] = -BEE_VEL;
+                        }
+                        
+                        // move
+                        bee_x[i][0] = bee_x[i][1] + bee_xv[i];
+                        bee_y[i][0] = bee_y[i][1] + bee_yv[i];
                     }
-                	
+                    
+                    // erase previous, draw current
+                    canvas.drawColor(Color.BLACK);
+                                        
+                    // wasp
+                    canvas.drawLine(wasp_x[0], wasp_y[0], wasp_x[1], wasp_y[1], wasp_p);
+                    
+                    //bees
+                    for(int i = 0; i < BEES; i++) {
+                        canvas.drawLine(bee_x[i][1], bee_y[i][1], bee_x[i][2], bee_y[i][2], bee_p);
+                        canvas.drawLine(bee_x[i][0], bee_y[i][0], bee_x[i][1], bee_y[i][1], bee_p);
+                    }
+                    
                     // all done
                     holder.unlockCanvasAndPost(canvas);
                     
@@ -203,7 +203,10 @@ public class SwarmView extends SurfaceView implements SurfaceHolder.Callback {
     public void surfaceDestroyed(SurfaceHolder sh) { }
     
     private int randPlusMinus(int r) {
-        float fr = (float)r;
-    	return (int)((Math.random() * fr) - (fr / 2F));
+    	// end of range = ±ceiling(r/2)
+        // truncated distribution: zero on end of range, double on zero
+    	//return (int)((Math.random() * r) - (r / 2D));
+    	// rounded distribution: half on end of range, normal on zero
+     	return (int)Math.round((Math.random() * r) - (r / 2D));
     }
 }
