3d653004430838761ed85aa8683135a333359c21
coax75ohm <coax75ohm@gmail.com>
2014-09-02 22:45:54 -0500

    use text edit dialog with bounds for number of bees and length of bee trail

--- src/org/coax/daydream/swarm/SwarmPreferenceActivity.java
+++ src/org/coax/daydream/swarm/SwarmPreferenceActivity.java
@@ -11,13 +11,30 @@ import android.preference.PreferenceFragment;
 import android.preference.PreferenceGroup;
 
 public class SwarmPreferenceActivity extends PreferenceActivity {
-    @Override
+
+    // TODO: find a cleaner way to do this from XML
+    private static String def_num_bees;
+    private static String min_num_bees;
+    private static String max_num_bees;
+    private static String def_bee_trail;
+    private static String min_bee_trail;
+    private static String max_bee_trail;
+
+	@Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
- 
+
         getFragmentManager().beginTransaction().replace(android.R.id.content, new SwarmPreferenceFragment()).commit();
+
+        // TODO: find a cleaner way to do this from XML
+        def_num_bees = getResources().getString(R.string.def_num_bees);
+        min_num_bees = getResources().getString(R.string.min_num_bees);
+        max_num_bees = getResources().getString(R.string.max_num_bees);
+        def_bee_trail = getResources().getString(R.string.def_bee_trail);
+        min_bee_trail = getResources().getString(R.string.min_bee_trail);
+        max_bee_trail = getResources().getString(R.string.max_bee_trail);
     }
- 
+
     public static class SwarmPreferenceFragment extends PreferenceFragment implements OnSharedPreferenceChangeListener {
         @Override
         public void onCreate(Bundle bundle) {
@@ -26,7 +43,7 @@ public class SwarmPreferenceActivity extends PreferenceActivity {
             addPreferencesFromResource(R.xml.preferences);
             getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
         }
-    
+
         @Override
         public void onResume() {
             super.onResume();
@@ -50,13 +67,42 @@ public class SwarmPreferenceActivity extends PreferenceActivity {
         }
 
         private void updatePreference(Preference preference) {
+            if(preference instanceof EditTextPreference) {
+                EditTextPreference edittextpreference = (EditTextPreference)preference;
+                EditTextCheckBounds(edittextpreference);
+                edittextpreference.setSummary(edittextpreference.getText());
+            }
+
             if(preference instanceof ListPreference) {
                 ListPreference listpreference = (ListPreference)preference;
                 listpreference.setSummary(listpreference.getEntry());
             }
-            if(preference instanceof EditTextPreference) {
-                EditTextPreference edittextpreference = (EditTextPreference)preference;
-                preference.setSummary(edittextpreference.getText());
+        }
+        
+        // TODO: find a cleaner way to do this from XML
+        private void EditTextCheckBounds(EditTextPreference edittextpreference) {
+            if(edittextpreference.getDialogTitle().equals("Number of Bees")) {
+                if(edittextpreference.getText().equals("")) {
+                    edittextpreference.setText(def_num_bees);
+                }
+                if(Integer.parseInt(edittextpreference.getText()) < Integer.parseInt(min_num_bees)) {
+                    edittextpreference.setText(min_num_bees);
+                }
+                if(Integer.parseInt(edittextpreference.getText()) > Integer.parseInt(max_num_bees)) {
+                    edittextpreference.setText(max_num_bees);
+                }
+            }
+
+            if(edittextpreference.getDialogTitle().equals("Bee Trail Length")) {
+                if(edittextpreference.getText().equals("")) {
+                    edittextpreference.setText(def_bee_trail);
+                }
+                if(Integer.parseInt(edittextpreference.getText()) < Integer.parseInt(min_bee_trail)) {
+                    edittextpreference.setText(min_bee_trail);
+                }
+                if(Integer.parseInt(edittextpreference.getText()) > Integer.parseInt(max_bee_trail)) {
+                    edittextpreference.setText(max_bee_trail);
+                }
             }
         }
     }
--- src/org/coax/daydream/swarm/SwarmView.java
+++ src/org/coax/daydream/swarm/SwarmView.java
@@ -1,4 +1,5 @@
 package org.coax.daydream.swarm;
+
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Canvas;
@@ -7,81 +8,116 @@ import android.graphics.Paint;
 import android.preference.PreferenceManager;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
 public class SwarmView extends SurfaceView implements SurfaceHolder.Callback {
+
     private int width;
     private int height;
     private Canvas canvas;
     private SurfaceHolder holder;
     private SharedPreferences sharedpreferences;
-    private static final int BEES     = 20;  // number of bees
+
+    //private static final int BEES     = 20;  // number of bees
     //private static final int REPEAT   = 3;   // number of time positions recorded
-    private int REPEAT;
     private static final int BEE_ACC  = 3;   // acceleration of bees
     private static final int WASP_ACC = 5;   // maximum acceleration of wasp
     private static final int BEE_VEL  = 11;  // maximum bee velocity
     private static final int WASP_VEL = 12;  // maximum wasp velocity
     private static final int BORDER   = 50;  // wasp won't go closer than this to the edges
     private static final int DELAY    = 40;  // millisecond delay between updates
-    int[] wasp_x = new int[2];
-    int[] wasp_y = new int[2];
+
+    private int BEES;
+    private int REPEAT;
+
+    int[] wasp_x;
+    int[] wasp_y;
     int wasp_xv;
     int wasp_yv;
-    int[][] bee_x = new int[BEES][11];
-    int[][] bee_y = new int[BEES][11];
-    int[] bee_xv  = new int[BEES];
-    int[] bee_yv  = new int[BEES];
+
+    int[][] bee_x;
+    int[][] bee_y;
+    int[] bee_xv;
+    int[] bee_yv;
+
     Paint wasp_p, bee_p;
+
     int dx, dy, distance;
+
     public SwarmView(Context context) {
         super(context);
+
         sharedpreferences = PreferenceManager.getDefaultSharedPreferences(context);
         holder = getHolder();
         holder.addCallback(this);
     }
+
     @Override
     public void surfaceCreated(SurfaceHolder sh) {
-        new Thread() {
+
+    	new Thread() {
             @Override
             public void run() {
+
                 canvas = null;
-                REPEAT = Integer.parseInt(sharedpreferences.getString("REPEAT", "3"));
+
+                BEES = Integer.parseInt(sharedpreferences.getString("BEES", "20"));
+                // add one here since the preference is for number of segments, not points
+                REPEAT = Integer.parseInt(sharedpreferences.getString("TRAIL", "2")) + 1;
+
                 // initialize wasp
+                wasp_x = new int[2];
+                wasp_y = new int[2];
                 wasp_x[0] = (int)(Math.random() * (width - 2 * BORDER)) + BORDER;
                 wasp_y[0] = (int)(Math.random() * (height - 2 * BORDER)) + BORDER;
                 wasp_x[1] = wasp_x[0];
                 wasp_y[1] = wasp_y[0];
+
                 wasp_xv = 0;
                 wasp_yv = 0;
+
                 wasp_p = new Paint();
                 //wasp_p.setAntiAlias(true);
                 wasp_p.setColor(Color.RED);
+
                 // initialize bees
+                int[][] bee_x = new int[BEES][REPEAT];
+                int[][] bee_y = new int[BEES][REPEAT];
+                int[] bee_xv  = new int[BEES];
+                int[] bee_yv  = new int[BEES];
                 for(int i = 0; i < BEES; i++) {
                     bee_x[i][0] = (int)(Math.random() * width);
                     bee_y[i][0] = (int)(Math.random() * height);
+
                     for(int j = 1; j < REPEAT; j++) {
                         bee_x[i][j] = bee_x[i][j-1];
                         bee_y[i][j] = bee_y[i][j-1];
                     }
+
                     bee_xv[i] = randPlusMinus(7);
                     bee_yv[i] = randPlusMinus(7);
                 }
+
                 bee_p = new Paint();
                 //bee_p.setAntiAlias(true);
                 bee_p.setColor(Color.YELLOW);
+
                 // animation loop
                 while (true) {
                     canvas = holder.lockCanvas();
+
                     if (canvas == null) {
                         break;
                     }
+
                     // <=- wasp -=>
                     // age the arrays
                     wasp_x[1] = wasp_x[0];
                     wasp_y[1] = wasp_y[0];
+
                     // accelerate
                     wasp_xv += randPlusMinus(WASP_ACC);
                     wasp_yv += randPlusMinus(WASP_ACC);
+
                     // speed limit checks
                     if (wasp_xv > WASP_VEL) {
                         wasp_xv = WASP_VEL;
@@ -95,9 +131,11 @@ public class SwarmView extends SurfaceView implements SurfaceHolder.Callback {
                     if (wasp_yv < -WASP_VEL) {
                         wasp_yv = -WASP_VEL;
                     }
+
                     // move
                     wasp_x[0] = wasp_x[1] + wasp_xv;
                     wasp_y[0] = wasp_y[1] + wasp_yv;
+
                     // bounce checks
                     if((wasp_x[0] < BORDER) || (wasp_x[0] > (width - BORDER - 1))) {
                         wasp_xv = -wasp_xv;
@@ -107,9 +145,11 @@ public class SwarmView extends SurfaceView implements SurfaceHolder.Callback {
                         wasp_yv = -wasp_yv;
                         wasp_y[0] += wasp_yv;
                     }
+
                     // don't let things settle down
                     bee_xv[(int)(Math.random() * BEES)] += randPlusMinus(3);
                     bee_yv[(int)(Math.random() * BEES)] += randPlusMinus(3);
+
                     // <=- bees -=>
                     for(int i = 0; i < BEES; i++) {
                         // age the arrays
@@ -117,6 +157,7 @@ public class SwarmView extends SurfaceView implements SurfaceHolder.Callback {
                             bee_x[i][j] = bee_x[i][j-1];
                             bee_y[i][j] = bee_y[i][j-1];
                         }
+
                         // accelerate
                         dx = wasp_x[1] - bee_x[i][1];
                         dy = wasp_y[1] - bee_y[i][1];
@@ -126,6 +167,7 @@ public class SwarmView extends SurfaceView implements SurfaceHolder.Callback {
                         }
                         bee_xv[i] += (dx * BEE_ACC) / distance;
                         bee_yv[i] += (dy * BEE_ACC) / distance;
+
                         // speed limit checks
                         if (bee_xv[i] > BEE_VEL) {
                             bee_xv[i] = BEE_VEL;
@@ -139,22 +181,28 @@ public class SwarmView extends SurfaceView implements SurfaceHolder.Callback {
                         if (bee_yv[i] < -BEE_VEL) {
                             bee_yv[i] = -BEE_VEL;
                         }
+
                         // move
                         bee_x[i][0] = bee_x[i][1] + bee_xv[i];
                         bee_y[i][0] = bee_y[i][1] + bee_yv[i];
                     }
+
                     // erase previous, draw current
                     canvas.drawColor(Color.BLACK);
+
                     // wasp
                     canvas.drawLine(wasp_x[0], wasp_y[0], wasp_x[1], wasp_y[1], wasp_p);
+
                     //bees
                     for(int i = 0; i < BEES; i++) {
                         for(int j = REPEAT - 1; j > 0; j--) {
                             canvas.drawLine(bee_x[i][j-1], bee_y[i][j-1], bee_x[i][j], bee_y[i][j], bee_p);
                         }
                     }
+
                     // all done
                     holder.unlockCanvasAndPost(canvas);
+
                     // rest for a bit
                     try {
                         Thread.sleep(DELAY);
@@ -165,13 +213,16 @@ public class SwarmView extends SurfaceView implements SurfaceHolder.Callback {
             }
         }.start();
     }
+
     @Override
     public void surfaceChanged(SurfaceHolder sh, int format, int width, int height) {
         this.width = width;
         this.height = height;
     }
+
     @Override
     public void surfaceDestroyed(SurfaceHolder sh) { }
+
     private int randPlusMinus(int r) {
          // end of range = ±ceiling(r/2)
          // truncated distribution: zero on end of range, double on zero
